1e2ea28853cda6448f41870a4b4a299395a49b82
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2021-10-24 06:29:41 +0300

    Update Program.cs

--- nanoframework-stm32-button/Program.cs
+++ nanoframework-stm32-button/Program.cs
@@ -7,53 +7,60 @@ namespace nanoframework_stm32_button
 {
     public class Program
     {
-        private static GpioController s_GpioController;
+        private static int LED_PIN;
+        private static int BUTTON_PIN;
+        private static GpioPin ledPin;
+        private static GpioPin buttonPin;        
+        private static GpioPinValue ledPinValue = GpioPinValue.High;
         public static void Main()
         {
-            s_GpioController = new GpioController();
-
-            // STM32F091RC: PA5 is LED_GREEN            
-            GpioPin led = s_GpioController.OpenPin(
-            PinNumber('A', 5));            
-            led.SetDriveMode(GpioPinDriveMode.Output);
-            led.Write(GpioPinValue.Low);
-
-
-            // Interrupt pin for RX message & TX done notification 
-            var InterruptGpioPin = s_GpioController.OpenPin(
-            PinNumber('C', 13));
-            InterruptGpioPin.SetDriveMode(GpioPinDriveMode.Input);
-
-            // InterruptGpioPin_ValueChanged
-            InterruptGpioPin.ValueChanged += UserButton_ValueChanged;
-
-            
-
-            Debug.WriteLine("Hello!");
-
-            while (true)
-            {
-                led.Toggle();
-                Thread.Sleep(125);
-                led.Toggle();
-                Thread.Sleep(125);
-                led.Toggle();
-                Thread.Sleep(125);
-                led.Toggle();
-                Thread.Sleep(525);
-            }
-
+            //source: Push button - Raspberry Pi
+            //https://docs.microsoft.com/en-us/samples/microsoft/windows-iotcore-samples/push-button/
+            // ST Nucleo64 F411RE: PA5 is LED_GREEN
+            LED_PIN = PinNumber('A', 5);
+            // ST Nucleo64 F411RE: PC13 is B1_USER
+            BUTTON_PIN = PinNumber('C', 13);
+            //Next as .NET for Raspberry Pi, Windows IoT Core
+            //Controller
+            var gpio = new GpioController();
+            //Init
+            ledPin = gpio.OpenPin(LED_PIN);
+            ledPin.SetDriveMode(GpioPinDriveMode.Output);
+            buttonPin = gpio.OpenPin(BUTTON_PIN);
+            buttonPin.SetDriveMode(GpioPinDriveMode.Input);
+            // Initialize LED to the OFF state by first writing a HIGH value
+            // We write HIGH because the LED is wired in a active LOW configuration
+            ledPin.Write(ledPinValue);            
+            // Check if input pull-up resistors are supported
+            if (buttonPin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
+                buttonPin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            else
+                buttonPin.SetDriveMode(GpioPinDriveMode.Input);
+            // Set a debounce timeout to filter out switch bounce noise from a button press
+            buttonPin.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+            // Register for the ValueChanged event so our buttonPin_ValueChanged 
+            // function is called when the button is pressed
+            buttonPin.ValueChanged += buttonPin_ValueChanged;
+            //Infinite
+            Thread.Sleep(Timeout.Infinite);
         }
-        private static void UserButton_ValueChanged(object sender, GpioPinValueChangedEventArgs e)
+
+        static void buttonPin_ValueChanged(object sender, GpioPinValueChangedEventArgs e)
         {
-            Debug.WriteLine(e.Edge.ToString());
-            
+            Debug.WriteLine("USER BUTTON (event) : " + ((e.Edge ==GpioPinEdge.RisingEdge)? "RisingEdge" : "FallingEdge"));            
+            // toggle the state of the LED every time the button is pressed
+            if (e.Edge == GpioPinEdge.FallingEdge)
+            {
+                ledPinValue = (ledPinValue == GpioPinValue.Low) ?
+                    GpioPinValue.High : GpioPinValue.Low;
+                ledPin.Write(ledPinValue);
+            }
         }
+            
         static int PinNumber(char port, byte pin)
         {
             if (port < 'A' || port > 'J')
                 throw new ArgumentException();
-
             return ((port - 'A') * 16) + pin;
         }
     }
